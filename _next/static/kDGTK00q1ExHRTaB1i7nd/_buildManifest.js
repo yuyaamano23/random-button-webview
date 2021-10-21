d008a2875f1ec1efdb63df4c5743713a461170bb
yuyaamano23 <yuyaamano23@users.noreply.github.com>
2021-10-21 13:46:42 +0000

    deploy: ab887f967356dfa537af0f2aa492e106baf7b828

---
+++ _next/static/kDGTK00q1ExHRTaB1i7nd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/99492b8413865e20907e.css","static/chunks/pages/index-17cd08ef036a8f37b69c.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
