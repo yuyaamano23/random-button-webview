df1235ea49f268195e762ce809f80754b23d1c52
yuyaamano23 <nba.chako.nba@icloud.com>
2021-10-22 00:40:36 +0900

    button追加

--- pages/button.tsx
+++ pages/button.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import React, { FC } from "react";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Button.module.css";
 
 const Button: FC = () => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.main}>
+				<div className={styles.btnSquareShadow}>click!!!</div>
 				<div>buttonのページだお</div>
 				<Link href="/">
 					<a>To button page</a>
