471fec6c06309c5373ddcbb1d1efb243dcccef80
yuyaamano23 <nba.chako.nba@icloud.com>
2021-10-24 14:04:34 +0900

    爆発したらreloadボタン表示

--- pages/button.tsx
+++ pages/button.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import React, { FC, useEffect, useState } from "react";
-import { useRecoilState, useSetRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 import { countState } from "../atoms";
 import styles from "../styles/Button.module.css";
+import Router from "next/router";
 
 const Button: FC = () => {
 	const [randnum, setRandnum] = useState<number>(0);
-	const [isFinished, setIsFinished] = useState<boolean>(false);
 	// recoil
 	const [count, setCount] = useRecoilState<number>(countState);
 	useEffect(() => {
@@ -15,14 +15,30 @@ const Button: FC = () => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.main}>
-				<h1>{randnum == count && <span>Bomb!!!!</span>}</h1>
-				<h3>count : {count}</h3>
-				<div
-					className={styles.btnSquareShadow}
-					onClick={() => setCount(count + 1)}
-				>
-					click!!!
-				</div>
+				{randnum == count ? (
+					<>
+						<h1>Bomb!!!!</h1>
+						<div
+							onClick={() => {
+								Router.reload();
+							}}
+						>
+							もういちど
+						</div>
+					</>
+				) : (
+					<>
+						<h3>count : {count}</h3>
+						<div
+							className={styles.btnSquareShadow}
+							onClick={() => {
+								setCount(count + 1);
+							}}
+						>
+							click!!!
+						</div>
+					</>
+				)}
 				<div>buttonのページだお</div>
 				<Link href="/">
 					<a>To main page</a>
